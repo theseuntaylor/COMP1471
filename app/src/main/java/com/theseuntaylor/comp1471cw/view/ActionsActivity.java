d1c39f20d7ffa7487484e287e3d4bba444d67f80
Solomon George-Taylor <seun.gtaylor@gmail.com>
2023-03-08 16:24:55 +0000

    Implemented screen pages.

--- app/src/main/java/com/theseuntaylor/comp1471cw/view/ActionsActivity.java
+++ app/src/main/java/com/theseuntaylor/comp1471cw/view/ActionsActivity.java
@@ -1,14 +1,16 @@
- package com.theseuntaylor.comp1471cw;
+package com.theseuntaylor.comp1471cw.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.theseuntaylor.comp1471cw.R;
+
+public class ActionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_actions);
     }
 }
